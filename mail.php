c858eaa06b2a4ea26c63cc37f5e1b05bcc53485f
adamray <adamrayw@users.noreply.github.com>
2021-09-10 16:56:00 +0700

    init

--- mail.php
+++ mail.php
@@ -19,13 +19,13 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'daffamzhen16@gmail.com';                     //SMTP username
-    $mail->Password   = 'sanholo11';                               //SMTP password
+    $mail->Username   = 'email@gmail.com';                     //SMTP username
+    $mail->Password   = 'passwordgmail';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('daffamzhen16@gmail.com', 'Admin');
+    $mail->setFrom('email@gmail.com', 'Admin');
     $mail->addAddress($_POST['email']);     //Add a recipient
 
     //Content
